6cad01ef4d7968f0198a0a1286823ff1458c4b6f
Alexandr Shusharin <alexshush@yandex.ru>
2022-12-09 19:12:56 +0300

    start

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,27 @@
 package ru.practicum.shareit.item.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.shareit.item.model.Item;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ItemDto {
+    String name;
+    String description;
+    boolean available;
+    long request;
+    public static ItemDto toItemDto(Item item) {
+        return new ItemDto(
+                item.getName(),
+                item.getDescription(),
+                item.isAvailable(),
+                item.getRequest() != null ? item.getRequest().getId() : null
+        );
+    }
 }
--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,15 @@
 package ru.practicum.shareit.item.model;
 
-/**
- * TODO Sprint add-controllers.
- */
+import lombok.Data;
+import ru.practicum.shareit.request.model.ItemRequest;
+import ru.practicum.shareit.user.model.User;
+
+@Data
 public class Item {
+    long id;
+    String name;
+    String description;
+    boolean available;
+    User owner;
+    ItemRequest request;
 }
