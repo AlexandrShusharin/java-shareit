fb2bf3ae00dcfe70f34729892a5cffb91c5bbc84
Alexandr Shusharin <alexshush@yandex.ru>
2023-01-26 13:35:22 +0300

    add: userController tests

---
+++ src/test/java/ru/practicum/shareit/user/service/UserServiceTest.java
@@ -0,0 +1,28 @@
+package ru.practicum.shareit.user.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void getUsers() {
+    }
+}
